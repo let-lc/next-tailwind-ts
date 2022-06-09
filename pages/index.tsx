e7897046bbc925934e1acfebcb538509725516a3
Liuming Chen <51524534+let-lc@users.noreply.github.com>
2022-06-08 20:48:03 -0400

    refactor: formatted with prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
-import { useEffect, useState } from "react";
-import Head from "next/head";
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
 
-type size = Record<"width" | "height", string>;
+type size = Record<'width' | 'height', string>;
 
 export default function Home() {
   const [cricle, setCricle] = useState<Record<string, size>>({
-    a: { width: "0", height: "0" },
-    b: { width: "0", height: "0" },
-    c: { width: "0", height: "0" },
+    a: { width: '0', height: '0' },
+    b: { width: '0', height: '0' },
+    c: { width: '0', height: '0' },
   });
 
   const randomSize = (): size => {
-    const size =
-      Math.round((Math.random() * 6 + Number.EPSILON) * 100) / 100 + "rem";
+    const size = Math.round((Math.random() * 6 + Number.EPSILON) * 100) / 100 + 'rem';
     return { width: size, height: size };
   };
 
@@ -24,30 +23,30 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col min-w-full min-h-screen bg-gray-50">
+    <div className="flex min-h-screen min-w-full flex-col bg-gray-50">
       <Head>
         <title>Next.js/TailwindCSS/TypeScript</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <div className="animate-bounce">
-          <div className="flex gap-x-2 animate-spin">
-            <div className="flex items-center justify-center w-24 h-24 rounded-full">
+          <div className="flex animate-spin gap-x-2">
+            <div className="flex h-24 w-24 items-center justify-center rounded-full">
               <div
-                className="bg-[#06B6D4] transition-all duration-150 rounded-full animate-bounce"
+                className="animate-bounce rounded-full bg-[#06B6D4] transition-all duration-150"
                 style={{ width: cricle.a.width, height: cricle.a.width }}
               />
             </div>
-            <div className="flex items-center justify-center w-24 h-24 rounded-full">
+            <div className="flex h-24 w-24 items-center justify-center rounded-full">
               <div
-                className="bg-black transition-all duration-150 rounded-full animate-bounce"
+                className="animate-bounce rounded-full bg-black transition-all duration-150"
                 style={{ width: cricle.b.width, height: cricle.b.width }}
               />
             </div>
-            <div className="flex items-center justify-center w-24 h-24 rounded-full">
+            <div className="flex h-24 w-24 items-center justify-center rounded-full">
               <div
-                className="bg-[#3178C6] transition-all duration-150 rounded-full animate-bounce"
+                className="animate-bounce rounded-full bg-[#3178C6] transition-all duration-150"
                 style={{ width: cricle.c.width, height: cricle.c.width }}
               />
             </div>
